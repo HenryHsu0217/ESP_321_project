e0ee4a9e8b782a53831b8858a5e53ad983ed62a2
henryhsu <henryhsu0217@gmail.com>
2023-10-09 10:36:13 +0800

    Just the basic of the InvertedDoublePendulum-v4 set up

--- ANN/ANN.py
+++ ANN/ANN.py
@@ -1,2 +1,17 @@
-##test
-print("Hello")
+import gym
+import time
+if __name__ == '__main__':
+    env = gym.make('InvertedDoublePendulum-v4',render_mode="human")
+    num_steps = 1500
+
+    obs = env.reset()
+    for step in range(num_steps):
+        # take random action
+        action = env.action_space.sample()
+        # apply the action
+        obs,a,b,c,d= env.step(action)
+        print(obs)
+        # Render the env
+        env.render()
+        time.sleep(0.001)
+    env.close()
