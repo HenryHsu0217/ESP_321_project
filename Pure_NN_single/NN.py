b07258efca5abcb99386d226441c5d8926941a39
henryhsu <henryhsu0217@gmail.com>
2023-10-20 17:28:04 +0800

    Setting up the neural network for the prue nerual network approach

---
+++ Pure_NN_single/NN.py
@@ -0,0 +1,21 @@
+import torch
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(4, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 1)
+        )
+    def forward(self, x):
+        out = self.mean_net(x)
+        return out
